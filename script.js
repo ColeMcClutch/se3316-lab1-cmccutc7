bce5b59459f25cff9f0dedd576a5144bb12d4e70
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-10-02 21:22:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,31 +12,7 @@ const resultsList1 = []
 //Gets References for currency
 const searchInput2 = document.getElementById('cuName')
 const resultsList2 = []
- 
- //country/currency type set
- const dataset =[
-   {country:'United States of America',code: 'USD'},
-   {country:'Canada',code: 'CAD'},
-   {country:'United Kingdom',code: 'GBP'},
-   {country:'Japan',code:'JPY'},
-   {country:'Austrailia',code:'AUD'},
-   {country:'Italy' ,code:'EUR'},
-   {country:'France' ,code:'EUR'},
-   {country:'Germany' ,code:'EUR'},
-   {country:'Spain' ,code:'EUR'},
-   {country: 'India',code:'INR'},
-   {country:'Brazil', code:'BRL'},
-   {country: 'China',code:'CNY'},
-   {country:'Russia',code:'RUB'},
-   {country:'South Africa', code:'ZAR'},
-   {country:'Mexico',code:'MXN'},
-   {country:'Argentina',code:'ARS'},
-   {country:'Saudi Arabia',code:'SAR'},
-   {country:'Nigeria',code:'NGN'},
-   {country:'South Korea',code:'KRW'},
-   {country:'Egypt',code:'EGP'}
- ]
- 
+  
 //node set
 const nodelist = document.querySelectorAll('div.column')
 const nodeArray = Array.from(nodelist) 
@@ -54,7 +30,7 @@ contentContainer.appendChild(newContentBlock);
 
 //content box searches
 function boxMatch1 (){
-  for (let i = 0; i < dataset.length; i++) {
+  for (let i = 0; i < nodeArray.length; i++) {
     const currentItem = nodeArray[i].getAttribute('id').toLowerCase() // Convert to lowercase for case-insensitive search
     if (currentItem.includes(searchInput1.value.toLowerCase())) {
       const container = document.createElement('div.content-block')
@@ -95,11 +71,6 @@ searchInput1.addEventListener("input",boxMatch1)
 searchInput2.addEventListener("input",boxMatch2)
 
 
-
-//Places functions into click operations
-button1.addEventListener('click', function(){performSearch1()})
-button2.addEventListener('click', function(){performSearch2()})
-
 //Function for shortening country tag
 function shortenText1() {
     const inputValue1 = searchInput1.value;
@@ -116,41 +87,6 @@ function shortenText1() {
     return searchInput1
   }
 
-//Function to perform search/ result filter
-function performSearch1() {
-    // Get the input element by its ID
-  
-    // Get the value entered by the user
-    const searchTerm1 = searchInput1.value.toLowerCase(); // Trim removes leading/trailing spaces
-    
-  //calls on shortenText for currency
-    if (searchTerm1.length>20){
-    alert("Input too long! Please shorten input")
-    }
-
-    // Perform the search
-    for (let i = 0; i < dataset.length; i++) {
-        const currentItem = dataset[i].country.toLowerCase(); // Convert to lowercase for case-insensitive search
-        if (currentItem.includes(searchTerm1.toLowerCase())) {
-            resultsList1.push(dataset[i].country +", ("+dataset[i].code+")");
-
-            // Limit the results to 5
-          if (resultsList1.length >= 5) {
-            break;
-          }
-        }
-    }
-
-    // Display matching results in an alert
-    if (resultsList1.length > 0) {
-        alert("Matching Results:\n" + resultsList1.join(", "));
-        resultsList1.length=0
-    } else {
-        alert("No matching results found.");
-    }   
-    resultsList1.length=0
-  }
-  
 
 
 //Currency type search
@@ -170,39 +106,4 @@ function shortenText2() {
     return searchInput2.value.toLowerCase()
   }
 
-//Function to perform search/filter results
-function performSearch2(){
-  
-  // Get the value entered by the user
-  const userInput2 = searchInput2.value.toLowerCase() // Convert to lowercase for search
-
-
-  //calls on shortenText for currency
-  if (userInput2.length>3){
-    alert("Input too long! Please shorten input")
-  }
-
-  // Iterate through dataset2 and find matching results
-  for (const value of dataset) {
-    const v1 = value.code.toLowerCase()
-    const v2 = v1.toLowerCase()
-    if (v2.includes(userInput2)) {
-      resultsList2.push(value.country + ": (" + value.code+")");
-
-      // Limit the results to 5
-      if (resultsList2.length >= 5) {
-        break;
-      }
-    }
-  }
-
-  // Display matching results in an alert
-  if (resultsList2.length > 0) {
-    alert("Matching results: " + resultsList2.join(", "));
-    resultsList2.length=0
-  } else {
-    alert("No matching results found.");
-  }
-
-}
 
